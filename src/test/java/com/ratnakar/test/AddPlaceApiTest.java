2f1e6e77b14db994a91a0cab8a3cc295cb8efe3a
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-07-22 17:09:55 +0530

    Add place Api Assertions

--- src/test/java/com/ratnakar/test/AddPlaceApiTest.java
+++ src/test/java/com/ratnakar/test/AddPlaceApiTest.java
@@ -1,14 +1,20 @@
 package com.ratnakar.test;
 
+import com.ratnakar.data.AddPlaceApiPayLoad;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.*;
 
 public class AddPlaceApiTest {
     @Test
     public void addPlaceApiTest(){
         // Validate if Add Place Api is Working as Expected.
+        // given() -> Using this method we build the API request with all input data & parameters including respective headers
+        // when() -> We use this method to submit the API resource for HTTP methods like Get, Post, Delete etc.
+        // then() -> We use this method for Validation of API response by providing the Assertions
+
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         given().log().all()
                 .queryParam("key", "qaclick123").header("Content-Type", "application/json")
@@ -24,7 +30,20 @@ public class AddPlaceApiTest {
                         "}\n")
                 .when().post("/maps/api/place/add/json")
                 .then().log().all().assertThat().statusCode(200);
-
     }
+    @Test
+    public void addPlaceApiTestAssertions(){
+        // Validate if Add Place Api is Working as Expected.
+        // equalTo(object) -> This static method is implemented from package org.hamcrest.CoreMatchers for comparing the response objects
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        given().log().all()
+                .queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body(AddPlaceApiPayLoad.addPlacePayLoad())
+                .when().post("/maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
+                .header("Server", "Apache/2.4.18 (Ubuntu)");
+    }
+
+    // Add place & fetch the placeID from api response. And for the same placeID update the details using update place api & Check if place is updated or not using get place api
 }
 
