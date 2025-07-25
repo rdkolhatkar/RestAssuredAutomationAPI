aabc2f72beabc96f48e05bc9f5438d1661ed9132
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-07-25 18:06:48 +0530

    Update place API

--- src/test/java/com/ratnakar/test/AddPlaceApiTest.java
+++ src/test/java/com/ratnakar/test/AddPlaceApiTest.java
@@ -2,12 +2,15 @@ package com.ratnakar.test;
 
 import com.ratnakar.data.AddPlaceApiPayLoad;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.*;
 
 public class AddPlaceApiTest {
+
+    String placeID;
     @Test
     public void addPlaceApiTest(){
         // Validate if Add Place Api is Working as Expected.
@@ -36,14 +39,31 @@ public class AddPlaceApiTest {
         // Validate if Add Place Api is Working as Expected.
         // equalTo(object) -> This static method is implemented from package org.hamcrest.CoreMatchers for comparing the response objects
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given().log().all()
+        String response = given().log().all()
                 .queryParam("key", "qaclick123").header("Content-Type", "application/json")
                 .body(AddPlaceApiPayLoad.addPlacePayLoad())
                 .when().post("/maps/api/place/add/json")
                 .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
-                .header("Server", "Apache/2.4.18 (Ubuntu)");
+                .header("Server", "Apache/2.4.18 (Ubuntu)").extract().response().asString();
+        // Extracting response as a String value in the response object to extract the placeID
+        System.out.println(response);
+        JsonPath jsonPath = new JsonPath(response);
+        placeID = jsonPath.getString("place_id");
     }
 
     // Add place & fetch the placeID from api response. And for the same placeID update the details using update place api & Check if place is updated or not using get place api
+    @Test
+    public void updatePlaceApiTest(){
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body("{\n" +
+                        "  \"place_id\": \""+placeID+"\",\n" +
+                        "  \"address\": \"70 Summer walk, USA\",\n" +
+                        "  \"key\": \"qaclick123\"\n" +
+                        "}")
+                .when().put("/maps/api/place/update/json")
+                .then().log().all().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
+    }
+    // Now we will be calling the GET Place API to check if our address is successfully updated or not
 }
 
