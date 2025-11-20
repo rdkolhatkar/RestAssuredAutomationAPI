4e0b68aa731745ef09e5a86aa0109c0c24f950ed
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-11-20 21:30:07 +0530

    added global properties

---
+++ src/main/java/com/ratnakar/cucumber/utils/TestUtils.java
@@ -0,0 +1,34 @@
+package com.ratnakar.cucumber.utils;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import java.io.*;
+import java.util.Properties;
+
+public class TestUtils {
+
+    RequestSpecification requestSpecBuilder;
+    public RequestSpecification requestSpecification() throws IOException {
+        PrintStream logPrintStream = new PrintStream(new FileOutputStream("logging.txt"));
+
+        requestSpecBuilder =  new RequestSpecBuilder().setBaseUri(getGlobalConfigurationProperties("appBaseUri"))
+                .addQueryParam("key", "qaclick123")
+                .addFilter(RequestLoggingFilter.logRequestTo(logPrintStream)) // adding filter for logging the API request
+                .addFilter(ResponseLoggingFilter.logResponseTo(logPrintStream)) // adding filter for logging the API response
+                .setContentType(ContentType.JSON)
+                .build();
+
+        return requestSpecBuilder;
+    }
+
+    public static String getGlobalConfigurationProperties(String key) throws IOException {
+        Properties properties = new Properties();
+        FileInputStream fileInputStream = new FileInputStream("src/main/resources/global.properties");
+        properties.load(fileInputStream);
+        return properties.getProperty(key);
+    }
+}
